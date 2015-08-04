052d34e66dd2ab1479f1e5cbb32aa52187940815
Daniel Park <dpark1985@gmail.com>
2015-08-04 22:15:56 +0900

    [ionic]

---
+++ rookie/www/js/app.js
@@ -0,0 +1,131 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', [
+    'ionic','ionic.service.core',
+    'ngCordova',
+    
+    'ionic.service.push',
+    'starter.controllers',
+    'starter.controllers'
+])
+.config(['$ionicAppProvider', function($ionicAppProvider) {
+    // Identify app
+    $ionicAppProvider.identify({
+        // The App ID (from apps.ionic.io) for the server
+        app_id: '2c5dbbdc',
+        // The public API key all services will use for this app
+        api_key: 'be81c1489526a9a70a2c654925e48b9ef68e1c9ef4d93b1b',
+        // Set the app to use development pushes
+
+    });
+}])
+.config(['$ionicConfigProvider', function( $ionicConfigProvider) {
+     $ionicConfigProvider.navBar.alignTitle('center');
+}])
+
+
+.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
+
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+    .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'MenuCtrl'
+    })
+
+    .state('app.login', {
+        url: '/login',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/menu/login.html',
+                controller: 'loginCtrl'
+            }
+        }
+    })
+
+    .state('app.adsQuery', {
+        url: '/adsQuery',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/menu/adsQuery.html',
+                controller: 'adsQueryCtrl'
+            }
+        }
+    })
+
+    .state('app.terms', {
+        url: '/terms',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/menu/terms.html'
+            }
+        }
+    })
+
+
+    .state('app.main', {
+        url: '/main',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/main.html',
+                controller: 'MainCtrl'
+            }
+        }
+    })
+
+    .state('app.sports', {
+        url: '/main/:sports',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/sports/sports.html',
+                controller: 'SportsCtrl'
+            }
+        }
+    })
+
+    .state('app.details', {
+        url: '/main/:sports/:category/:details',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/sports/details.html',
+                controller: 'DetailCtrl'
+            }
+        }
+    })
+
+
+
+
+    .state('app.single', {
+        url: '/playlists/:playlistId',
+        views: {
+            'detailContent': {
+                templateUrl: 'templates/playlist.html',
+                controller: 'PlaylistCtrl'
+            }
+        }
+    });
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/main');
+});
---
+++ rookie/www/js/controllers.js
@@ -0,0 +1,352 @@
+angular.module('starter.controllers', [])
+
+.controller('MenuCtrl', function($state, $scope, $rootScope, $location) {
+
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
+
+    console.log($location.url());
+
+    
+
+    $scope.login = function() {
+        $state.go('app.login');
+    };
+
+    $scope.adsQuery = function() {
+        $state.go('app.adsQuery');
+    };
+
+    $scope.terms = function() {
+        $state.go('app.terms');
+    };
+
+
+
+
+
+
+})
+.controller('loginCtrl', function($scope, $rootScope, $state, $ionicModal, $timeout, $http) {
+    $scope.loginData = {};
+    $scope.validCheck = 'positive';
+    
+
+
+    $scope.doLogin = function() {
+        var emailV = $scope.loginData.userID.split('@');
+        //console.log('Doing login', $scope.loginData);
+        if(emailV.length == 1 || $scope.loginData.userPW == ''){
+            $scope.validCheck = 'assertive';
+        } else {
+            $scope.validCheck = 'positive';
+            $http.post('http://52.69.2.200/happ/testing', {
+                status: 'login',
+                login: $scope.loginData.userID,
+                password: $scope.loginData.userPW
+            })
+            .success(function(data, status, headers, config) {
+                if(data){
+                    $state.go('app.main');
+                    $rootScope.isLogin = true;
+                } else {
+                    $scope.validCheck = 'assertive';
+                }
+            })
+            .error(function(data, status, headers, config) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+            });
+        }
+    };
+
+
+
+
+    
+
+    $ionicModal.fromTemplateUrl('templates/menu/reRegister.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+        }).then(function(modal) {
+            $scope.modalReRegister = modal;
+    });
+
+    $scope.reRegister = function() {
+        $scope.modalReRegister.show();
+    }
+
+    $scope.closeReRegister = function() {
+        $scope.modalReRegister.hide();
+    };
+
+    //Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function() {
+        $scope.modalReRegister.remove();
+    });
+
+    $scope.reRegisterData = {};
+    $scope.doReRegister = function() {
+        console.log('Doing reRegister', $scope.reRegisterData);
+
+    };
+
+
+
+
+
+
+    $ionicModal.fromTemplateUrl('templates/menu/register.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+        }).then(function(modal) {
+            $scope.modalRegister = modal;
+    });
+
+    $scope.register = function() {
+        $scope.modalRegister.show();
+    };
+
+    $scope.closeRegister = function() {
+        $scope.modalRegister.hide();
+    };
+    //Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function() {
+        $scope.modalRegister.remove();
+    });        
+
+    $scope.registerData = {};
+    $scope.regiValidCheck = 'positive';
+    $scope.doRegister = function() {
+        //console.log('Doing register', $scope.registerData);
+        var regiEmailV = $scope.registerData.userID.split('@');
+        if(regiEmailV.length == 2 && $scope.registerData.userPW == $scope.registerData.userPWC){
+            $scope.regiValidCheck = 'positive';
+
+
+            $http.post('http://52.69.2.200/happ/testingUserID', {
+                login: $scope.registerData.userID
+            })
+            .success(function(data, status, headers, config) {
+                if(data){
+                    $scope.regiValidCheck = 'assertive';
+                } else {
+                    $scope.regiValidCheck = 'positive';
+
+                    $http.post('http://52.69.2.200/happ/testing', {
+                        status: 'register',
+                        login: $scope.registerData.userID,
+                        password: $scope.registerData.userPW
+                    })
+                    .success(function(data, status, headers, config) {
+                        if(data){
+                            $state.go('app.main');
+                            $rootScope.isLogin = true;
+                            $timeout(function() {
+                                $scope.closeRegister();
+                            }, 1000);
+
+                        } else {
+                            $scope.regiValidCheck = 'assertive';
+                        }
+                    })
+                    .error(function(data, status, headers, config) {
+                        // called asynchronously if an error occurs
+                        // or server returns response with an error status.
+                    });
+                }
+            })
+            .error(function(data, status, headers, config) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+            });
+
+
+
+
+
+
+        } else {
+            $scope.regiValidCheck = 'assertive';
+        }
+    };
+
+
+
+
+
+
+
+
+
+
+
+})
+
+.controller('adsQueryCtrl', function($scope, $http, $timeout) {
+    $scope.adsQueryData = {};
+    $scope.doAdsQuery = function() {
+        console.log('Doing adsQuery', $scope.adsQueryData);
+
+        $http.post('http://52.69.2.200/query', {
+            login: $scope.adsQueryData.userEmail,
+            subject: $scope.adsQueryData.title,
+            context: $scope.adsQueryData.content
+        })
+        .success(function(data, status, headers, config) {
+            
+            $timeout(function() {
+                $state.go('app.login');
+            }, 1000);
+        })
+        .error(function(data, status, headers, config) {
+
+        });        
+    };
+
+
+
+
+})
+.controller('MainCtrl', function($scope, $ionicSlideBoxDelegate, $timeout) {
+
+
+
+
+
+
+
+})
+.controller('SportsCtrl', function($scope, $location, $http, $ionicTabsDelegate) {
+    var locationArr = $location.url().split('/');
+
+    //Selected Sports
+    $scope.curSports = locationArr[3];
+
+
+    $http.get('http://52.69.2.200/'+ $scope.curSports +'/competitions')
+    .success(function(data, status, headers, config) {
+        $scope.competitionItems = data;
+    })
+    .error(function(data, status, headers, config) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+    });
+
+    $scope.competitions = function() {
+        $ionicTabsDelegate.select(0);
+
+
+        $http.get('http://52.69.2.200/'+ $scope.curSports +'/competitions')
+        .success(function(data, status, headers, config) {
+            $scope.competitionItems = data;
+        })
+        .error(function(data, status, headers, config) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+        });
+    };
+
+    $scope.courts = function() {
+        $ionicTabsDelegate.select(1);
+
+
+        $scope.items = null;
+        $http.get('http://52.69.2.200/'+ $scope.curSports +'/courts')
+        .success(function(data, status, headers, config) {
+            $scope.courtItems = data;
+        })
+        .error(function(data, status, headers, config) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+        });
+    }
+
+    $scope.clubs = function() {
+        $ionicTabsDelegate.select(2);
+
+
+        $http.get('http://52.69.2.200/'+ $scope.curSports +'/clubs')
+        .success(function(data, status, headers, config) {
+            $scope.clubItems = data;
+        })
+        .error(function(data, status, headers, config) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+        });
+    }
+})
+.controller('DetailCtrl', function ($scope, $rootScope, $location, $http, $sce) {
+
+    var locationArr = $location.url().split('/');
+    $rootScope.curSport = locationArr[3]; //name of the sports
+    $rootScope.curCategory = locationArr[4]; //competitions | courts | clubs
+    $rootScope.curId = locationArr[5]; //ID of the item
+
+    console.log($rootScope.curCategory);
+
+
+    $scope.contentActive = '';
+    $scope.basicInfoActive = 'active';
+    $scope.imageActive = '';
+
+    $scope.templates = {
+        "content" : "templates/sports/sportsTemplates/"+$rootScope.curCategory+"/content.html",
+        "basicInfo" : "templates/sports/sportsTemplates/"+$rootScope.curCategory+"/basicInfo.html",
+        "images" : "templates/sports/sportsTemplates/"+$rootScope.curCategory+"/images.html" 
+    };
+
+    $scope.activeInclude = $scope.templates.basicInfo;
+
+
+    $http.get('http://52.69.2.200/happ/testingDetails/'+$rootScope.curCategory+'/'+$rootScope.curId)
+    .success(function(data, status, headers, config) {
+        $scope.item = data;
+
+        if($rootScope.curCategory == 'competitions'){
+            $scope.item[0].eventInfo = $sce.trustAsHtml($scope.item[0].eventInfo);
+            $scope.imageURL =  "http://52.69.2.200/uploads/"+$scope.item[0].eventImg;
+            $scope.mainTitle = $scope.item[0].eventTitle;
+        } else if ($rootScope.curCategory == 'clubs'){
+            $scope.item[0].clubInfo = $sce.trustAsHtml($scope.item[0].clubInfo);
+            $scope.imageURL =  "http://52.69.2.200/uploads/"+$scope.item[0].clubImg; 
+            $scope.mainTitle = $scope.item[0].clubTitle;
+        } else {
+            $scope.item[0].courtInfo = $sce.trustAsHtml($scope.item[0].courtInfo);
+            $scope.imageURL =  "http://52.69.2.200/uploads/"+$scope.item[0].courtImg;
+            $scope.mainTitle = $scope.item[0].courtTitle; 
+        }   
+        console.log(data);        
+    })
+    .error(function(data, status, headers, config) {
+
+    }); 
+
+
+
+    $scope.contentClick = function() {
+        $scope.contentActive = 'active';
+        $scope.basicInfoActive = '';
+        $scope.imageActive = '';
+        $scope.activeInclude = $scope.templates.content;
+    };
+    $scope.infoClick = function() {
+        $scope.contentActive = '';
+        $scope.basicInfoActive = 'active';
+        $scope.imageActive = '';
+        $scope.activeInclude = $scope.templates.basicInfo;
+
+    };
+    $scope.imgClick = function() {
+        $scope.contentActive = '';
+        $scope.basicInfoActive = '';
+        $scope.imageActive = 'active';
+        $scope.activeInclude = $scope.templates.images;
+    }
+
+
+});
